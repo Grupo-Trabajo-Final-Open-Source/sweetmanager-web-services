fddcdad43bb5ac0c28ca7c557dd052a1490f8ba8
Mauricio <u202211572@upc.edu.pe>
2024-06-04 15:09:01 -0500

    feat(payment): added CreateSubscriptionCommandFromResourceAssembler class.

---
+++ src/main/java/com/git/sweetmanager/payment/interfaces/rest/transform/CreateSubscriptionCommandFromResourceAssembler.java
@@ -0,0 +1,9 @@
+package com.git.sweetmanager.payment.interfaces.rest.transform;
+
+import com.git.sweetmanager.payment.domain.model.commands.CreateSubscriptionCommand;
+
+public class CreateSubscriptionCommandFromResourceAssembler {
+    public  static CreateSubscriptionCommand toCommandFromResource(CreateSubscriptionCommand resource) {
+        return new CreateSubscriptionCommand(resource.title(), resource.price(), resource.description());
+    }
+}
