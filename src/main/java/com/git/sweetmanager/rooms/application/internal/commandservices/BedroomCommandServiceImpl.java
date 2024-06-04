58c39c230e6031f8027407b9f96c511deec52d11
aaron <U202211552@upc.edu.pe>
2024-06-04 15:16:37 -0500

    feat(rooms): updated resources and transform.

--- src/main/java/com/git/sweetmanager/rooms/application/internal/commandservices/BedroomCommandServiceImpl.java
+++ src/main/java/com/git/sweetmanager/rooms/application/internal/commandservices/BedroomCommandServiceImpl.java
@@ -4,12 +4,20 @@ import com.git.sweetmanager.rooms.domain.model.aggregates.Bedroom;
 import com.git.sweetmanager.rooms.domain.model.commands.CreateBedroomCommand;
 import com.git.sweetmanager.rooms.domain.model.commands.UpdateBedroomCommand;
 import com.git.sweetmanager.rooms.domain.services.BedroomCommandService;
+import com.git.sweetmanager.rooms.infraestructure.persistence.mongo.repositories.BedroomRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class BedroomCommandServiceImpl implements BedroomCommandService {
+
+    private final BedroomRepository repository;
+
+    public BedroomCommandServiceImpl(BedroomRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
     public Optional<Bedroom> handle(CreateBedroomCommand command) {
         return Optional.empty();
--- src/main/java/com/git/sweetmanager/rooms/application/internal/commandservices/BookingCommandServiceImpl.java
+++ src/main/java/com/git/sweetmanager/rooms/application/internal/commandservices/BookingCommandServiceImpl.java
@@ -4,12 +4,20 @@ import com.git.sweetmanager.rooms.domain.model.aggregates.Booking;
 import com.git.sweetmanager.rooms.domain.model.commands.CreateBookingCommand;
 import com.git.sweetmanager.rooms.domain.model.commands.UpdateBookingCommand;
 import com.git.sweetmanager.rooms.domain.services.BookingCommandService;
+import com.git.sweetmanager.rooms.infraestructure.persistence.mongo.repositories.BookingRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
 public class BookingCommandServiceImpl implements BookingCommandService {
+
+    private final BookingRepository bookingRepository;
+
+    public BookingCommandServiceImpl(BookingRepository bookingRepository) {
+        this.bookingRepository = bookingRepository;
+    }
+
     @Override
     public Optional<Booking> handle(CreateBookingCommand command) {
         return Optional.empty();
