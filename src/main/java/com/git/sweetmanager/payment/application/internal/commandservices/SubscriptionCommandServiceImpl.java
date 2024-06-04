cd8bb3ad1987386cf8e734bc21633cc08e060b4a
Mauricio <u202211572@upc.edu.pe>
2024-06-04 14:47:49 -0500

    feat(payment): implemented SubscriptionCommandServiceImpl class for subscription validation at creation.

---
+++ src/main/java/com/git/sweetmanager/payment/application/internal/commandservices/SubscriptionCommandServiceImpl.java
@@ -0,0 +1,38 @@
+package com.git.sweetmanager.payment.application.internal.commandservices;
+
+import com.git.sweetmanager.payment.domain.model.aggregates.Subscription;
+import com.git.sweetmanager.payment.domain.model.commands.CreateSubscriptionCommand;
+import com.git.sweetmanager.payment.domain.services.SubscriptionCommandService;
+import com.git.sweetmanager.payment.infrastructure.persistence.mongo.repositories.SubscriptionRepository;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class SubscriptionCommandServiceImpl implements SubscriptionCommandService {
+
+    private final SubscriptionRepository subscriptionRepository;
+
+    public SubscriptionCommandServiceImpl(SubscriptionRepository subscriptionRepository) {
+        this.subscriptionRepository = subscriptionRepository;
+    }
+
+    @Override
+    public Optional<Subscription> handle(CreateSubscriptionCommand command) {
+        if(command.title() == null || command.title().isEmpty()) {
+            throw new IllegalArgumentException("Title cannot be null or empty");
+        }
+        if(command.price() == null || command.price() == 0){
+            throw new IllegalArgumentException("Price cannot be null or empty");
+        }
+        if(command.description() == null || command.description().isEmpty()) {
+            throw new IllegalArgumentException("Description cannot be null or empty");
+        }
+
+        var subscription = new Subscription(command);
+
+        return Optional.of(subscription);
+    }
+
+}
