28b05dd7a2d8cf309946c0af07223d6eef04fc3c
aaron <U202211552@upc.edu.pe>
2024-06-04 14:17:59 -0500

    feat(rooms): implemented facade.

--- src/main/java/com/git/sweetmanager/rooms/infraestructure/persistence/mongo/repositories/BookingRepository.java
+++ src/main/java/com/git/sweetmanager/rooms/infraestructure/persistence/mongo/repositories/BookingRepository.java
@@ -2,6 +2,8 @@ package com.git.sweetmanager.rooms.infraestructure.persistence.mongo.repositorie
 
 import com.git.sweetmanager.rooms.domain.model.aggregates.Booking;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookingRepository extends MongoRepository<Booking, Long> {
 }
