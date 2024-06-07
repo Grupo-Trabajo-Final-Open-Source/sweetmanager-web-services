dadb66dd95d1a137e7d8e9375ea25956d156d783
Mauricio <mauriciorojasanchez@gmail.com>
2024-06-06 21:49:56 -0500

    chore: updated NotificationRepository for handling and validation information.

--- src/main/java/com/git/sweetmanager/communication/infrastructure/persistence/mongo/repositories/NotificationRepository.java
+++ src/main/java/com/git/sweetmanager/communication/infrastructure/persistence/mongo/repositories/NotificationRepository.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 
+@Repository
 public interface NotificationRepository extends MongoRepository<Notification, Long> {
     Optional<Notification> findById(int id);
 
