36a99f0e512105570f0f56a9e451949daee040dd
Mauricio <mauriciorojasanchez@gmail.com>
2024-06-06 20:14:51 -0500

    feat(iam): added SequenceException for handling errors when inserting new id for every collection.

---
+++ src/main/java/com/git/sweetmanager/shared/domain/entities/SequenceException.java
@@ -0,0 +1,8 @@
+package com.git.sweetmanager.shared.domain.entities;
+
+public class SequenceException extends RuntimeException {
+
+    public SequenceException(String message) {
+        super(message);
+    }
+}
