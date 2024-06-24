7524f4ae74754a270dd7c181717826b6878e554f
Mauricio <mauriciorojasanchez@gmail.com>
2024-06-24 15:32:10 -0500

    feat: added CORS policy support.

--- src/test/java/com/git/sweetmanager/SweetManagerApplicationTests.java
+++ src/test/java/com/git/sweetmanager/SweetManagerApplicationTests.java
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SweetManagerApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
-
+//    @Test
+//    void contextLoads() {
+//    }
 }
