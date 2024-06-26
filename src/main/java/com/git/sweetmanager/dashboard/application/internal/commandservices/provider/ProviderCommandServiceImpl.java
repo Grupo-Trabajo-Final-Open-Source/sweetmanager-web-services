3f4f6887a132f0483e1b2ac70c5ecd356e1afa23
Fernando Salgado <u202212023@upc.edu.pe>
2024-06-26 16:04:50 -0500

    feat(dashboard): provider command service updated.

--- src/main/java/com/git/sweetmanager/dashboard/application/internal/commandservices/provider/ProviderCommandServiceImpl.java
+++ src/main/java/com/git/sweetmanager/dashboard/application/internal/commandservices/provider/ProviderCommandServiceImpl.java
@@ -2,6 +2,8 @@ package com.git.sweetmanager.dashboard.application.internal.commandservices.prov
 
 import com.git.sweetmanager.dashboard.domain.model.aggregates.Provider;
 import com.git.sweetmanager.dashboard.domain.model.commands.provider.CreateProviderCommand;
+import com.git.sweetmanager.dashboard.domain.model.commands.provider.DeleteProviderCommand;
+import com.git.sweetmanager.dashboard.domain.model.commands.provider.UpdateProviderCommand;
 import com.git.sweetmanager.dashboard.domain.services.provider.ProviderCommandService;
 import com.git.sweetmanager.dashboard.infrastructure.persistence.mongo.repositories.provider.ProviderRepository;
 import org.springframework.stereotype.Service;
@@ -32,5 +34,36 @@ public class ProviderCommandServiceImpl implements ProviderCommandService {
         providerRepository.save(provider);
 
         return Optional.of(provider);
+    }
+
+
+    @Override
+    public Optional<Provider> handle(UpdateProviderCommand command) {
+        if(command.name() == null || command.name().isEmpty())
+            throw new IllegalArgumentException("Name is required");
+        if(command.contact() == null || command.contact().isEmpty())
+            throw new IllegalArgumentException("Contact is required");
+        if(command.address() == null || command.address().isEmpty())
+            throw new IllegalArgumentException("Address is required");
+        if(command.product() == null || command.product().isEmpty())
+            throw new IllegalArgumentException("Product is required");
+
+        var provider = new Provider(command);
+
+        providerRepository.save(provider);
+
+        return Optional.of(provider);
+    }
+
+    @Override
+    public void handle(DeleteProviderCommand command) {
+        if(!providerRepository.existsById(command.id())){
+            throw new IllegalArgumentException("Provider doesn't exist");
+        }
+        try{
+            providerRepository.deleteById(command.id());
+        } catch(Exception e){
+            throw new IllegalArgumentException("Error occurred while deleting provider: " + e.getMessage());
+        }
     }
 }
--- src/main/java/com/git/sweetmanager/dashboard/domain/services/provider/ProviderCommandService.java
+++ src/main/java/com/git/sweetmanager/dashboard/domain/services/provider/ProviderCommandService.java
@@ -2,9 +2,13 @@ package com.git.sweetmanager.dashboard.domain.services.provider;
 
 import com.git.sweetmanager.dashboard.domain.model.aggregates.Provider;
 import com.git.sweetmanager.dashboard.domain.model.commands.provider.CreateProviderCommand;
+import com.git.sweetmanager.dashboard.domain.model.commands.provider.DeleteProviderCommand;
+import com.git.sweetmanager.dashboard.domain.model.commands.provider.UpdateProviderCommand;
 
 import java.util.Optional;
 
 public interface ProviderCommandService {
     Optional<Provider> handle(CreateProviderCommand command);
+    void handle(DeleteProviderCommand command);
+    Optional<Provider> handle(UpdateProviderCommand command);
 }
